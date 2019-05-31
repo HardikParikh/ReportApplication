08002a15e5f35ab0010bbe6ad6a6f28ccd048847
hardikparikh <h.y.parikh@gmail.com>
2019-05-30 23:36:28 -0700

    Added check for accepting and processing run time arguments

--- ReportApp/src/com/pmac/dashboard/DashboardData.java
+++ ReportApp/src/com/pmac/dashboard/DashboardData.java
@@ -5,7 +5,13 @@ import com.pmac.dashboard.util.Operations;
 public class DashboardData {
 
 	public static void main(String[] args){
-		System.out.println("Minimum weather difference: "+Operations.findMinimum("src/com/pmac/dashboard/data/w_data.txt",1,2,0));
-		System.out.println("Minimum goal difference:"+Operations.findMinimum("src/com/pmac/dashboard/data/soccer.txt",6,8,1));	
+		
+		if(args.length > 0 && args.length == 4){
+			System.out.println("Minimum weather difference: "+Operations.findMinimum(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]),Integer.parseInt(args[3])));			
+		}
+		else{	
+			System.out.println("Minimum weather difference: "+Operations.findMinimum("src/com/pmac/dashboard/data/w_data.txt",1,2,0));
+			System.out.println("Minimum goal difference: "+Operations.findMinimum("src/com/pmac/dashboard/data/soccer.txt",6,8,1));	
+		}
 	}
 }
